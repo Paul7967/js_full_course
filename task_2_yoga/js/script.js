b03508d4e58dd205def9d5ea0e7e937b02334c51
Paul7967 <postatochnikov@yandex.ru>
2019-12-27 21:14:30 +0500

    Create tabs (task_2_yoga)

--- task_2_yoga/js/script.js
+++ task_2_yoga/js/script.js
@@ -1,3 +1,53 @@
 window.addEventListener('DOMContentLoaded', () => {
-	
-})
+	'use strict';
+
+	let tab = document.querySelectorAll('.info-header-tab'),
+		info = document.querySelector('.info-header'),
+		tabContent = document.querySelectorAll('.info-tabcontent');
+
+
+	const showTab = (tabNum) => {
+		console.log(tabContent[tabNum].classList.contains('show'));
+		if (!(tabContent[tabNum].classList.contains('show'))) {
+			console.log('Первый запуск');
+			for ( let i = 0; i < tabContent.length; i++ ) {
+				console.log(i);
+				if (i == tabNum) {
+					tabContent[i].classList.remove('hide');
+					tabContent[i].classList.add('show');
+				} else {
+					tabContent[i].classList.remove('show');
+					tabContent[i].classList.add('hide');
+				};
+				
+			}
+		}
+	};
+
+	function hideTabContent(a) {
+		for ( let i = a; i < tabContent.length; i++ ) {
+			tabContent[i].classList.remove('show');
+			tabContent[i].classList.add('hide');
+		}
+	};
+
+	// hideTabContent(1);
+	showTab(0);
+	// function showTabContent(b) {
+		
+	// };
+
+	info.addEventListener('click', function(event) {
+		let { target } = event;
+		if (target && target.classList.contains('info-header-tab')) {
+			for (let i = 0; i < tab.length; i++) {
+				if (target == tab[i]) {
+					showTab(i);
+					// hideTabContent(0);
+					// showTabContent(i);
+					// break;
+				}
+			}
+		}
+	});
+});
